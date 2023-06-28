9a5f676881fa8161f4000ad4496b85b69e4e8bb5
Bryan Borgeson <bryborge@gmail.com>
2023-06-28 16:50:46 -0700

    dry up tests

--- tests/fizzbuzz_test.py
+++ tests/fizzbuzz_test.py
@@ -1,26 +1,23 @@
 from fizzbuzz import create
 
+# Arrange, Act
+FIZZ_BUZZ_ARR = create(15)
+
 
 def test_fizzbuzz_replaces_all_numbers_divisible_by_three_with_fizz():
-    # Arrange, Act
-    fb = create(15)
     # Assert
-    assert fb[2] == "Fizz"
-    assert fb[5] == "Fizz"
-    assert fb[8] == "Fizz"
-    assert fb[11] == "Fizz"
+    assert FIZZ_BUZZ_ARR[2] == "Fizz"
+    assert FIZZ_BUZZ_ARR[5] == "Fizz"
+    assert FIZZ_BUZZ_ARR[8] == "Fizz"
+    assert FIZZ_BUZZ_ARR[11] == "Fizz"
 
 
 def test_fizzbuzz_replace_all_numbers_divisible_by_five_with_buzz():
-    # Arrange, Act
-    fb = create(15)
     # Assert
-    assert fb[4] == "Buzz"
-    assert fb[9] == "Buzz"
+    assert FIZZ_BUZZ_ARR[4] == "Buzz"
+    assert FIZZ_BUZZ_ARR[9] == "Buzz"
 
 
 def test_fizzbuzz_replaces_all_numbers_divisible_by_three_and_five_with_fizzbuzz():
-    # Arrange, Act
-    fb = create(15)
     # Assert
-    assert fb[14] == "FizzBuzz"
+    assert FIZZ_BUZZ_ARR[14] == "FizzBuzz"
